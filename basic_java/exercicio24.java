c0e3e0306cc7978dacc636095e2bc1b2bdbb8310
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-08-17 11:41:25 -0300

    Update exercicio24.java

--- basic_java/exercicio24.java
+++ basic_java/exercicio24.java
@@ -4,29 +4,27 @@ public class exercicio24 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int contador = 0;
-        int menor_perfeito = Integer.MAX_VALUE; // Inicializa com um valor alto
-        boolean encontrou_perfeito = false; // Flag para verificar se encontramos algum número perfeito
+        int menor_perfeito = Integer.MAX_VALUE;
+        boolean encontrou_perfeito = false;
 
         while (contador < 10) {
             System.out.println("Digite um número inteiro: ");
             int numero = scanner.nextInt();
             int somaDivisores = 0;
 
-            // Verifica os divisores próprios do número
             for (int x = 1; x < numero; x++) {
                 if (numero % x == 0) {
                     somaDivisores += x;
                 }
             }
-
-            // Verifica se o número é perfeito
+            
             if (somaDivisores == numero) {
                 encontrou_perfeito = true;
                 if (numero < menor_perfeito) {
                     menor_perfeito = numero;
                 }
-                contador++; // Incrementa o contador apenas se encontrar um número perfeito
             }
+        contador++;
         }
 
         if (encontrou_perfeito) {
