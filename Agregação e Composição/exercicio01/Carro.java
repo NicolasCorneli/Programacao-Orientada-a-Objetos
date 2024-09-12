f039ab10c89dba0b96f780e03e2e95741e731deb
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-09-11 21:43:51 -0300

    Create Carro.java

---
+++ Agregação e Composição/exercicio01/Carro.java
@@ -0,0 +1,17 @@
+package exer1;
+
+public class Carro {
+    private Motor motor;
+    
+    public Carro(){
+        this.motor = new Motor(1,"Gasolina");
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+}
