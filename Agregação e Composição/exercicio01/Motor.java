e30c467ad9647531fde3026fde80b250ccec0433
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-09-11 21:44:04 -0300

    Create Motor.java

---
+++ Agregação e Composição/exercicio01/Motor.java
@@ -0,0 +1,29 @@
+package exer1;
+
+public class Motor {
+    private int potencia;
+    private String tipoCombustivel;
+
+    public Motor(int potencia, String tipoCombustivel){
+        this.potencia = potencia;
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    
+}
