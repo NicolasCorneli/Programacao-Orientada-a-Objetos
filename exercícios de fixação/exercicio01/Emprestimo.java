1209f66935bb92db3e50027c16a1ca611c1dbd10
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-09-21 20:01:32 -0300

    Update Emprestimo.java

--- exercícios de fixação/exercicio01/Emprestimo.java
+++ exercícios de fixação/exercicio01/Emprestimo.java
@@ -1,32 +1,45 @@
-package exercicio1;
-
 public class Emprestimo {
     private Livro livro;
     private Usuario usuario;
-    private String data_emprestimo;
-    private String data_devolucao;
+    private String dataEmprestimo;
+    private String dataDevolucao;
 
-    public Emprestimo(Livro livro, Usuario usuario, String dataE){
+    public Emprestimo(Livro livro, Usuario usuario, String dataEmprestimo,String dataDevolucao){
         this.livro = livro;
         this.usuario = usuario;
-        this.data_emprestimo = dataE;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
     }
 
     public Livro getLivro() {
         return livro;
     }
 
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
     public Usuario getUsuario() {
         return usuario;
     }
 
-    public String getData_emprestimo() {
-        return data_emprestimo;
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 
-    public String getData_devolucao() {
-        return data_devolucao;
+    public String getDataEmprestimo() {
+        return dataEmprestimo;
     }
 
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
 
+    public String getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
 }
