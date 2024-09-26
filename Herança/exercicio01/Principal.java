f46bbec88ad54a176690739988617ed34feb83aa
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-09-25 22:02:21 -0300

    Create Principal.java

---
+++ Herança/exercicio01/Principal.java
@@ -0,0 +1,10 @@
+package exercicio01;
+
+public class Principal {
+    public static void main(String[] args){
+        Aluno a1 = new Aluno("Braga",21,6469);
+
+        a1.exibir();
+    }
+    
+}
