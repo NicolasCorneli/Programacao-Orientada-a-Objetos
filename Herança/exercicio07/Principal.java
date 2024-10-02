d280a6ab42cce4a9438f103f5078ecfcf81ce9bd
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-10-02 16:09:35 -0300

    Create Principal.java

---
+++ Herança/exercicio07/Principal.java
@@ -0,0 +1,9 @@
+package exercicio07;
+public class Principal {
+    public static void main(String[] args){
+        Mamifero m1 = new Mamifero("K",20,"Selva","Fotossíntese");
+
+        m1.exibir();
+    }
+    
+}
