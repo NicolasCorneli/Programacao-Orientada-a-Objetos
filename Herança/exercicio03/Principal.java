bc42a23485470305d7003ac1d5068b848c6421e7
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-10-02 15:06:36 -0300

    Create Principal.java

---
+++ Herança/exercicio03/Principal.java
@@ -0,0 +1,9 @@
+package exercicio03
+public class Principal {
+    public static void main(String[] args){
+        Cachorro c1 = new Cachorro("Cachorro", "Husky");
+
+        c1.exibir();
+    }
+    
+}
