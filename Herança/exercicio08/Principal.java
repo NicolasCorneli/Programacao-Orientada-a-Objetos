ed49d8b3ed6230a98def3ab51ba79341efb08451
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-10-02 16:17:34 -0300

    Create Principal.java

---
+++ Herança/exercicio08/Principal.java
@@ -0,0 +1,9 @@
+package exercicio08;
+public class Principal {
+    public static void main(String[] args){
+        Carro c1 = new Carro(3000, "Honda Civic");
+
+        c1.exibirCapacidadeTanque();
+    }
+    
+}
