9bef815d9358331170c93761cb48c4d918c67f2b
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-10-02 15:19:48 -0300

    Create Gerente.java

---
+++ Herança/exercicio04/Gerente.java
@@ -0,0 +1,11 @@
+package exercicio04;
+public class Gerente extends Funcionario {
+    
+    public Gerente(double salario){
+        super(salario);
+    }
+    
+    public void exibir(){
+        super.exibir();
+    }
+}
