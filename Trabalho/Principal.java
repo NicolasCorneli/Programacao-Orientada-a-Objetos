5e32d2974b4b7e9a39ffb17c55dc0f1af6280f5f
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-10-27 16:53:33 -0300

    Update Principal.java

--- Trabalho/Principal.java
+++ Trabalho/Principal.java
@@ -10,17 +10,20 @@ public class Principal {
         gestaoFrota.adicionarMotorista(motorista1);
 
         // Criando veículos
-        CarrosCompactos carroCompacto = new CarrosCompactos(1, "Tesla", "Model 3", LocalDate.of(2022, 5, 14), 50, 200);
+        CarrosCompactos carroCompacto = new CarrosCompactos(1, "Tesla", "Model 3", LocalDate.of(2022, 5, 14),200.0,200.0);
         gestaoFrota.adicionarVeiculo(carroCompacto);
 
         // Criando eletropostos
         Eletroposto eletroposto1 = new Eletroposto(1, "São Paulo", 10, 2);
+        Eletroposto eletroposto2 = new Eletroposto(2, "Rio de Janeiro", 15, 2);
         gestaoFrota.adicionarEletroposto(eletroposto1);
+        gestaoFrota.adicionarEletroposto(eletroposto2);
 
         // Criando e realizando viagem
-        Viagem viagem = new Viagem(motorista1, carroCompacto, 190, "Rio de Janeiro");
-        viagem.realizarViagem();
+        Viagem viagem = new Viagem(motorista1, carroCompacto, 300, "Rio de Janeiro");
+        viagem.realizarViagem(gestaoFrota.getListaEletropostos());
 
+        System.out.println(carroCompacto.getAutonomiaAtual());
         gestaoFrota.veiculosComBaixaAutonomia();
         
     }
