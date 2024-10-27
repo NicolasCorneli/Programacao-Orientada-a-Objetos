6c3ad5bf840750609ecb299b758a8d3f1f11fe63
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-10-27 14:21:48 -0300

    Create Eletroposto.java

---
+++ Trabalho/Eletroposto.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class Eletroposto {
+    private int id;
+    private String local; // cidade ou estrada
+    private int vagasDisponiveis;
+    private double tempoCarregamento;
+
+    public Eletroposto(int id, String local, int vagasDisponiveis, double tempoCarregamento){
+        this.id = id;
+        this.local = local;
+        this.vagasDisponiveis = vagasDisponiveis;
+        this.tempoCarregamento = tempoCarregamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public int getVagasDisponiveis() {
+        return vagasDisponiveis;
+    }
+
+    public void setVagasDisponiveis(int vagasDisponiveis) {
+        this.vagasDisponiveis = vagasDisponiveis;
+    }
+
+    public double getTempoCarregamento() {
+        return tempoCarregamento;
+    }
+
+    public void setTempoCarregamento(double tempoCarregamento) {
+        this.tempoCarregamento = tempoCarregamento;
+    }
+
+    public boolean temVagaDisponivel() {
+        return vagasDisponiveis > 0;
+    }
+
+    public void ocuparVaga() {
+        if (temVagaDisponivel()) vagasDisponiveis--;
+    }
+
+    public void liberarVaga() {
+        vagasDisponiveis++;
+        System.out.println("Liberando vaga e continuando viagem!");
+    }
+}
