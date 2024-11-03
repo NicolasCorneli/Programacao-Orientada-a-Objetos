e03cb3ef406818254bdcb45b0431e6c70f341e92
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-11-03 03:39:55 -0300

    Update Viagem.java

--- Trabalho/Viagem.java
+++ Trabalho/Viagem.java
@@ -14,6 +14,55 @@ public class Viagem{
         this.distancia = distancia;
         this.destino = destino;
         this.paradasEletroposto = new ArrayList<>();
+        motorista.registrarViagem(this);
+    }
+
+    public Motorista getMotorista() {
+        return motorista;
+    }
+
+    public void setMotorista(Motorista motorista) {
+        this.motorista = motorista;
+    }
+
+    public VeiculosEletricos getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(VeiculosEletricos veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public ArrayList<Eletroposto> getParadasEletroposto() {
+        return paradasEletroposto;
+    }
+
+    public void setParadasEletroposto(ArrayList<Eletroposto> paradasEletroposto) {
+        this.paradasEletroposto = paradasEletroposto;
+    }
+
+    public ArrayList<Eletroposto> getVerificaEletroposto() {
+        return verificaEletroposto;
+    }
+
+    public void setVerificaEletroposto(ArrayList<Eletroposto> verificaEletroposto) {
+        this.verificaEletroposto = verificaEletroposto;
     }
 
     public void adicionarParada(Eletroposto e) {
