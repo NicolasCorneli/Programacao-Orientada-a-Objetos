d60b7a4d6fb7a2b833a5364d9e755180b7f59f30
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-11-13 21:34:35 -0300

    Create exercicio2.py

---
+++ python/exercicio2.py
@@ -0,0 +1,18 @@
+class Calculadora:
+    def adicao(self,n1,n2):
+        print(n1+n2)
+    
+    def subtracao(self,n1,n2):
+        print(n1-n2)
+    
+    def multiplicacao(self,n1,n2):
+        print(n1*n2)
+    
+    def divisao(self,n1,n2):
+        print(n1/n2)
+
+c1 = Calculadora()
+c1.adicao(6,3)
+c1.subtracao(6,3)
+c1.multiplicacao(6,3)
+c1.divisao(6,3)
